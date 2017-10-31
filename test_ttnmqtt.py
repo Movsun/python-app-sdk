667ffc440dc570d19fbb43631eaa4cfe05a95419
emmlejeail <manu.lejeail@gmail.com>
2017-10-31 16:55:57 +0100

    MQTT: Add callbacks for connect and close

--- test_ttnmqtt.py
+++ test_ttnmqtt.py
@@ -5,8 +5,8 @@ from ttnmqtt.ttnmqtt import MQTTClient as mqtt
 
 
 def test_connection():
-    ttn_client = mqtt('appid', 'appeui', 'psw')
-    ttn_client.startBackground()
+    ttn_client = mqtt('appid', 'psw')
+    ttn_client.start()
     try:
         ttn_client.connect(
             os.getenv("MQTT_HOST", "localhost"),
@@ -16,18 +16,50 @@ def test_connection():
     assert ttn_client.connectFlag == 1
 
 
+def test_connect_callback():
+    ttn_client = mqtt('appid', 'psw')
+
+    def connectcallback(rc, client):
+        if(rc == 0):
+            print("connected")
+        else:
+            print("not connected")
+        assert client.connectFlag == 1
+    ttn_client.setConnectCallback(connectcallback)
+    ttn_client.start()
+    try:
+        ttn_client.connect(
+            os.getenv("MQTT_HOST", "localhost"),
+            os.getenv("MQTT_PORT", 1883))
+    except:
+        print("Error connecting")
+
+
 def test_disconnection():
-    ttn_client = mqtt('appid', 'appeui', 'psw')
+    ttn_client = mqtt('appid', 'psw')
     ttn_client.connect(
         os.getenv("MQTT_HOST", "localhost"),
         os.getenv("MQTT_PORT", 1883))
-    ttn_client.disconnect()
+    ttn_client.close()
     assert ttn_client.disconnectFlag == 1
 
 
-def test_set_separate_behaviors():
-    ttn_client1 = mqtt('appid', 'appeui', 'psw')
-    ttn_client2 = mqtt('appid', 'appeui', 'psw')
+def test_close_callback():
+    ttn_client = mqtt('appid', 'psw')
+    ttn_client.connect(
+        os.getenv("MQTT_HOST", "localhost"),
+        os.getenv("MQTT_PORT", 1883))
+
+    def closecallback(rc, client):
+        print("closing connection")
+        assert client.disconnectFlag == 1
+    ttn_client.setCloseCallback(closecallback)
+    ttn_client.close()
+
+
+def test_connect_behavior():
+    ttn_client1 = mqtt('appid', 'psw')
+    ttn_client2 = mqtt('appid', 'psw')
 
     def on_connect(client, userdata, flags, rc):
         client.subscribe("appid/devices/devid/down")
@@ -36,7 +68,7 @@ def test_set_separate_behaviors():
         print(msg)
         msgReceived = msg['payload_raw']
         assert msgReceived == "AQ=="
-        client.stopBackground()
+        client.stop()
 
     ttn_client1.setConnectBehavior(on_connect)
     ttn_client2.setConnectBehavior(on_connect)
@@ -47,7 +79,7 @@ def test_set_separate_behaviors():
     ttn_client2.connect(
         os.getenv("MQTT_HOST", "localhost"),
         os.getenv("MQTT_PORT", 1883))
-    ttn_client2.startBackground()
+    ttn_client2.start()
     ttn_client1.publish(
         'devid',
         '{"port": 1, "confirmed": "false", "payload_raw": "AQ=="}')
@@ -56,9 +88,40 @@ def test_set_separate_behaviors():
         '{"port": 1, "confirmed": "false", "payload_raw": "AQ=="}')
 
 
+def test_downlink_field_msg():
+    ttn_client1 = mqtt('appid', 'psw')
+    ttn_client2 = mqtt('appid', 'psw')
+
+    def on_connect(client, userdata, flags, rc):
+        client.subscribe("appid/devices/devid/down")
+
+    def msgcallback(msg, client):
+        print(msg)
+        msgReceived = msg.payload_raw
+        assert msgReceived == "AQ=="
+        client.stop()
+
+    ttn_client1.setConnectBehavior(on_connect)
+    ttn_client2.setConnectBehavior(on_connect)
+    ttn_client2.setUplinkCallback(msgcallback)
+    ttn_client1.connect(
+        os.getenv("MQTT_HOST", "localhost"),
+        os.getenv("MQTT_PORT", 1883))
+    ttn_client2.connect(
+        os.getenv("MQTT_HOST", "localhost"),
+        os.getenv("MQTT_PORT", 1883))
+    ttn_client2.start()
+    ttn_client1.publish(
+        'devid',
+        {"port": 1, "payload_raw": "AQ=="})
+    ttn_client2.publish(
+        'devid',
+        {"port": 1, "payload_raw": "AQ=="})
+
+
 def test_message_callback():
-    ttn_client1 = mqtt('appid', 'appeui', 'psw')
-    ttn_client2 = mqtt('appid', 'appeui', 'psw')
+    ttn_client1 = mqtt('appid', 'psw')
+    ttn_client2 = mqtt('appid', 'psw')
 
     ttn_client1.connect(
         os.getenv("MQTT_HOST", "localhost"),
@@ -76,10 +139,10 @@ def test_message_callback():
     def msgcallback(msg, client):
         print(msg)
         assert msg == '{"port": 1, "confirmed": false, "payload_raw": "AQ=="}'
-        client.stopBackground()
+        client.stop()
 
     ttn_client2.setUplinkCallback(msgcallback)
-    ttn_client2.startBackground()
+    ttn_client2.start()
     ttn_client1.publish(
         'devid',
         '{"port": 1, "confirmed": false, "payload_raw": "AQ=="}')
@@ -89,7 +152,7 @@ def test_message_callback():
 
 
 def test_publish_callback():
-    ttn_client1 = mqtt('appid', 'appeui', 'psw')
+    ttn_client1 = mqtt('appid', 'psw')
 
     ttn_client1.connect(
         os.getenv("MQTT_HOST", "localhost"),
@@ -103,22 +166,22 @@ def test_publish_callback():
     def publishcallback(mid, client):
         print(mid)
         assert mid == 1
-        client.stopBackground()
+        client.stop()
 
     ttn_client1.setDownlinkCallback(publishcallback)
-    ttn_client1.startBackground()
+    ttn_client1.start()
     ttn_client1.publish(
         'devid',
         '{"port": 1, "confirmed": false, "payload_raw": "AQ=="}')
 
 
 def test_connect_error():
-    ttn_client = mqtt('appid', 'appeui', 'psw')
-    ttn_client.startBackground()
+    ttn_client = mqtt('appid', 'psw')
+    ttn_client.start()
     try:
         ttn_client.connect(
             os.getenv("MQTT_HOST", "localhost"),
             os.getenv("MQTT_PORT", 8883))
     except:
-        ttn_client.stopBackground()
+        ttn_client.stop()
     assert ttn_client.connectFlag == 0
--- ttnmqtt/ttnmqtt.py
+++ ttnmqtt/ttnmqtt.py
@@ -1,20 +1,44 @@
 import paho.mqtt.client as mqtt
 from events import Events
+import base64
+from collections import namedtuple
 import json
 
 
+def _json_object_hook(d): return namedtuple('MSG', d.keys())(*d.values())
+
+
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+
+class DownlinkMessage:
+    def __init__(self, port, conf=False, sched=None, praw=None, pfields=None):
+
+        self.port = port
+        if praw:
+            self.praw = praw
+        if pfields:
+            self.pfields = pfields
+        if conf:
+            self.confirmed = conf
+        if sched:
+            self.schedule = sched
+
+    def obj2json(self):
+        json_msg = json.dumps(self.__dict__)
+        return str(json_msg)
+
+
 class MyEvents(Events):
-    __events__ = ("uplink_msg", "downlink_msg", "connection", "disconnection")
+    __events__ = ("uplink_msg", "downlink_msg", "connect", "close")
 
 
 class MQTTClient:
 
-    def __init__(self, APPID, APPEUI, PSW):
+    def __init__(self, appID, appAccessKey):
         self.__client = mqtt.Client()
-        self.__APPID = APPID
-        self.__APPEUI = APPEUI
-        self.__PSW = PSW
-        self.__currentMSG = {}
+        self.__APPID = appID
+        self.__APPACCESSKEY = appAccessKey
         self.__events = MyEvents()
         self.connectFlag = 1
         self.disconnectFlag = 0
@@ -23,51 +47,55 @@ class MQTTClient:
     def connect(self, address='eu.thethings.network', port=1883):
         if self.__client.on_connect is None:
             self.__client.on_connect = self._onConnect()
-        self.__client.on_publish = self._onPublish()
+        self.__client.on_publish = self._onDownlink()
         self.__client.on_message = self._onMessage()
-        if self.__client.on_disconnect is None:
-            self.__client.on_disconnect = self._onDisconnect()
+        self.__client.on_disconnect = self._onClose()
 
-        self.__client.username_pw_set(self.__APPID, self.__PSW)
+        self.__client.username_pw_set(self.__APPID, self.__APPACCESSKEY)
         self.__client.connect(address, port, 5000)
 
     def _onConnect(self):
         def on_connect(client, userdata, flags, rc):
             if(rc == 0):
                 print('CONNECTED AND SUBSCRIBED')
-                client.subscribe('+/devices/+/up'.format(self.__APPEUI))
+                client.subscribe(self.__APPID+'/devices/+/up')
                 self.connectFlag = 1
             else:
                 print('ERROR CONNECTING')
                 self.connectFlag = 0
+            if self.__events.connect:
+                self.__events.connect(rc, client=self)
         return on_connect
 
-    def _onDisconnect(self):
-        def on_disconnect(client, userdata, rc):
+    def _onClose(self):
+        def on_close(client, userdata, rc):
             if rc != 0:
                 print('UNEXPECTED DISCONNECTION')
                 self.disconnectFlag = 0
             else:
                 self.disconnectFlag = 1
                 print('DISCONNECTED')
-        return on_disconnect
+            if self.__events.close:
+                self.__events.close(rc, client=self)
+        return on_close
 
     def _onMessage(self):
         def on_message(client, userdata, msg):
             print('MESSAGE RECEIVED')
-            j_msg = json.loads(msg.payload.decode('utf-8'))
-            self.__currentMSG = j_msg
+            j_msg = str(json.dumps(json.loads(msg.payload.decode('utf-8'))))
+            obj = json2obj(j_msg)
+            print("received message from "+str(obj.dev_id))
             if self.__events.uplink_msg:
-                self.__events.uplink_msg(j_msg, client=self)
+                self.__events.uplink_msg(obj, client=self)
         return on_message
 
-    def _onPublish(self):
-        def on_publish(client, userdata, mid):
+    def _onDownlink(self):
+        def on_downlink(client, userdata, mid):
             print('MSG PUBLISHED')
             self.midCounter = mid
             if self.__events.downlink_msg:
                 self.__events.downlink_msg(mid, client=self)
-        return on_publish
+        return on_downlink
 
     def setUplinkCallback(self, callback):
         self.__events.uplink_msg += callback
@@ -75,28 +103,42 @@ class MQTTClient:
     def setDownlinkCallback(self, callback):
         self.__events.downlink_msg += callback
 
+    def setConnectCallback(self, callback):
+        self.__events.connect += callback
+
+    def setCloseCallback(self, callback):
+        self.__events.close += callback
+
     def setConnectBehavior(self, connect):
         self.__client.on_connect = connect
 
-    def start(self):
+    def startForever(self):
         print('LOOP STARTED')
         self.__client.loop_forever()
 
-    def startBackground(self):
+    def start(self):
         print('LOOP STARTED BACKGROUND')
         self.__client.loop_start()
 
-    def stopBackground(self):
+    def stop(self):
         print('LOOP STOPPED BACKGROUND')
         self.__client.loop_stop()
-        self.disconnect()
+        self.close()
 
-    def disconnect(self):
+    def close(self):
         self.__client.disconnect()
         self.connectFlag = 0
 
-    def publish(self, devID, msg):
-        result = self.__client.publish(
+    def publish(self, devID, payload, port=1, conf=False, sched="replace"):
+        message = DownlinkMessage(port, conf, sched)
+        if isinstance(payload, str):
+            message.payload_raw = payload
+        else:
+            message.payload_fields = payload
+
+        msg = message.obj2json()
+
+        print("publishing message to "+devID+": "+msg)
+        self.__client.publish(
             self.__APPID+'/devices/'+devID+'/down',
             msg)
-        print("Message will be published on the next uplink message")
